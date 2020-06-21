6c25b989cc7b6cb68832610b52b32ab005aca236
Anthony Ngo <anthonyngo@pop-os.localdomain>
2020-06-21 14:36:04 -0700

    Changed to functional component

--- src/components/ColorView/ColorView.js
+++ src/components/ColorView/ColorView.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react'
 import clipboard from './clip.png'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-class ColorView extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: '',
-            copied: false
-        }
-    }
-
-   
-
-    render() {
+const ColorView = ({ colorView }) => {
         const colorList = () => {
             return(
-                this.props.colorView.map(color => {
+                colorView.map((color, index) => {
                     return(
                         <div>
-                            <div className='copy'>
+                            <div key={index} className='copy'>
                                 <h1>{color}</h1>
                                 <CopyToClipboard text={color}>
                                     <img src={clipboard} alt=""/>
@@ -37,7 +26,6 @@ class ColorView extends Component {
             </div>
         )
     }
-}
 
 export default ColorView
 
