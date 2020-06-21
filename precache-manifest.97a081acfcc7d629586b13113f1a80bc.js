9dc2f73f377a4c621fe28aacf17a6f12e07814f5
Anthony Ngo <anthonyngo@pop-os.localdomain>
2020-06-20 21:05:04 -0700

    Updates

---
+++ precache-manifest.97a081acfcc7d629586b13113f1a80bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab46aa25420fbf6245c7c80c8f93312",
+    "url": "/aesthetically/index.html"
+  },
+  {
+    "revision": "a88eda923020215b8baa",
+    "url": "/aesthetically/static/css/main.2ccfd64c.chunk.css"
+  },
+  {
+    "revision": "9807d2b14b379efb24f0",
+    "url": "/aesthetically/static/js/2.7f33a4e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aesthetically/static/js/2.7f33a4e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88eda923020215b8baa",
+    "url": "/aesthetically/static/js/main.bdd2e86f.chunk.js"
+  },
+  {
+    "revision": "8fe44f1415d1b6d66ab9",
+    "url": "/aesthetically/static/js/runtime-main.9af4b6a1.js"
+  }
+]);
